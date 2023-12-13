ccab45f86dc33c14073f4f4990c84e1b767f2578
Deepak Kanshetti <kanshettid@gmail.com>
2023-12-13 22:41:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // write js code here if required
+function updateTimer(){
+	const currentDate = new Date();
+	const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()}`;
+	const formattedTime = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`
+
+	document.getElementById('timer').innerHTML = `${formattedDate} ,${formattedTime}`;
+}
+
+updateTimer();
